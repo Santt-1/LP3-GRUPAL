f0768ba22db61a5a0daaefb11c42b68b5c8aface
AnggeloLucciano21 <anggelolucciano21@gmail.com>
2026-03-04 10:50:32 -0500

    sexooooo

--- DrinkGo-backend/src/main/java/DrinkGo/DrinkGo_backend/controller/PosController.java
+++ DrinkGo-backend/src/main/java/DrinkGo/DrinkGo_backend/controller/PosController.java
@@ -7,15 +7,45 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import DrinkGo.DrinkGo_backend.dto.pos.AbrirCajaRequest;
 import DrinkGo.DrinkGo_backend.dto.pos.AnularVentaRequest;
 import DrinkGo.DrinkGo_backend.dto.pos.CerrarCajaRequest;
 import DrinkGo.DrinkGo_backend.dto.pos.CrearVentaPosRequest;
 import DrinkGo.DrinkGo_backend.dto.pos.MovimientoCajaRequest;
-import DrinkGo.DrinkGo_backend.entity.*;
-import DrinkGo.DrinkGo_backend.repository.*;
+import DrinkGo.DrinkGo_backend.entity.CajasRegistradoras;
+import DrinkGo.DrinkGo_backend.entity.CategoriasGasto;
+import DrinkGo.DrinkGo_backend.entity.DetalleVentas;
+import DrinkGo.DrinkGo_backend.entity.DocumentosFacturacion;
+import DrinkGo.DrinkGo_backend.entity.MetodosPago;
+import DrinkGo.DrinkGo_backend.entity.MovimientosCaja;
+import DrinkGo.DrinkGo_backend.entity.Negocios;
+import DrinkGo.DrinkGo_backend.entity.PagosVenta;
+import DrinkGo.DrinkGo_backend.entity.Productos;
+import DrinkGo.DrinkGo_backend.entity.Sedes;
+import DrinkGo.DrinkGo_backend.entity.SesionesCaja;
+import DrinkGo.DrinkGo_backend.entity.Ventas;
+import DrinkGo.DrinkGo_backend.repository.CajasRegistradorasRepository;
+import DrinkGo.DrinkGo_backend.repository.CategoriasGastoRepository;
+import DrinkGo.DrinkGo_backend.repository.DetalleVentasRepository;
+import DrinkGo.DrinkGo_backend.repository.DocumentosFacturacionRepository;
+import DrinkGo.DrinkGo_backend.repository.MetodosPagoRepository;
+import DrinkGo.DrinkGo_backend.repository.MovimientosCajaRepository;
+import DrinkGo.DrinkGo_backend.repository.NegociosRepository;
+import DrinkGo.DrinkGo_backend.repository.PagosVentaRepository;
+import DrinkGo.DrinkGo_backend.repository.SedesRepository;
+import DrinkGo.DrinkGo_backend.repository.SesionesCajaRepository;
+import DrinkGo.DrinkGo_backend.repository.VentasRepository;
 import DrinkGo.DrinkGo_backend.service.FacturacionService;
 import DrinkGo.DrinkGo_backend.service.PosService;
 
@@ -239,7 +269,7 @@ public class PosController {
                     .orElseThrow(() -> new RuntimeException("Negocio no encontrado"));
 
             // Obtener la primera sede del negocio
-            List<Sedes> sedes = sedesRepo.findByNegocio_Id(negocioId);
+            List<Sedes> sedes = sedesRepo.findByNegocioId(negocioId);
             if (sedes.isEmpty()) {
                 return ResponseEntity.badRequest()
                         .body(Map.of("error", "El negocio no tiene sedes configuradas"));
